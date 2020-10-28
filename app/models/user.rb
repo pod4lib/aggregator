1fed46f96bbd58051540e637b05edf1f201e8914
Hector Correa <hector_correa@brown.edu>
2020-10-28 15:10:54 -0400

    Links a user to an organization (so that in the future uploads can be linked to an org)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@
 class User < ApplicationRecord
   rolify
   include Devise::JWT::RevocationStrategies::Allowlist
+  belongs_to :organization
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
