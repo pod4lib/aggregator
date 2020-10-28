b766a5c617286d18e21ddb4af4d6ebec7e529d7e
Hector Correa <hector_correa@brown.edu>
2020-10-28 15:41:00 -0400

    Allow/deny access to add, edit, and delete organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_070809) do
+ActiveRecord::Schema.define(version: 2020_10_28_145400) do
 
   create_table "allowlisted_jwts", force: :cascade do |t|
     t.string "jti", null: false
@@ -86,7 +86,9 @@ ActiveRecord::Schema.define(version: 2020_10_28_070809) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
