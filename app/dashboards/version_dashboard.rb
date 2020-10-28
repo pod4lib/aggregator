3c2df78a1a3bfbe4fbf9557c651c989c01d5ae78
Jack Reed <phillipjreed@gmail.com>
2020-10-28 16:24:01 -0600

    Hack to use PaperTrail w/ Administrate

---
+++ app/dashboards/version_dashboard.rb
@@ -0,0 +1,74 @@
+require "administrate/base_dashboard"
+
+class PaperTrail::VersionDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    item: Field::Polymorphic,
+    id: Field::Number,
+    event: Field::String,
+    whodunnit: Field::String,
+    object: Field::Text,
+    created_at: Field::DateTime,
+    object_changes: Field::Text,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+  item
+  id
+  event
+  whodunnit
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+  item
+  id
+  event
+  whodunnit
+  object
+  created_at
+  object_changes
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+  item
+  event
+  whodunnit
+  object
+  object_changes
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how versions are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(version)
+  #   "PaperTrail::Version ##{version.id}"
+  # end
+end
+class VersionDashboard < PaperTrail::VersionDashboard
