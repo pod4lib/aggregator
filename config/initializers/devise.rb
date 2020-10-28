2b146c5368b410835d42472a844484c588ab9d67
Chris Beer <cabeer@stanford.edu>
2020-10-28 11:41:41 -0700

    Make the login form + flow look nice

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Devise::SessionsController.layout 'devise'
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
