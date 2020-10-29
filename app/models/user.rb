2997472355c4f1ff23918fe632c7873491f147fb
Jessie Keck <jessie.keck@gmail.com>
2020-10-29 13:11:20 -0700

    Remove devise-jwt (but keep table/model for later use)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,9 @@
 # :nodoc:
 class User < ApplicationRecord
   rolify
-  include Devise::JWT::RevocationStrategies::Allowlist
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: self
+         :recoverable, :rememberable, :validatable
 end
