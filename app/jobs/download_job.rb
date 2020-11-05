a1abfd1826531b9fbb077b644ec960b9c356828c
Chris Beer <cabeer@stanford.edu>
2020-11-05 10:16:40 -0800

    Also produce MARCXML dumps

--- app/jobs/download_job.rb
+++ app/jobs/download_job.rb
@@ -5,17 +5,27 @@
 class DownloadJob < ApplicationJob
   # rubocop:disable Metrics/AbcSize
   def perform(organization)
-    file = Tempfile.new(organization.slug, binmode: true)
+    xmlfile = Tempfile.new("#{organization.slug}-marcxml")
+    xmlwriter = MARC::XMLWriter.new(xmlfile)
+    file = Tempfile.new("#{organization.slug}-marc21", binmode: true)
     organization.default_stream.uploads.each do |upload|
       upload.each_marc_record_metadata.each do |record|
         file.write(record.marc.to_marc)
+        xmlwriter.write(record.marc)
       end
     end
     file.rewind
     organization.full_dump_binary.attach(io: file, filename: "#{organization.slug}_#{Time.zone.today}")
+
+    # we would use xmlwriter#close, but it annoyingly also closes the file handle on us..
+    xmlfile.write('</collection>')
+    xmlfile.rewind
+    organization.full_dump_xml.attach(io: xmlfile, filename: "#{organization.slug}_#{Time.zone.today}")
   ensure
     file.close
     file.unlink
+    xmlfile.close
+    xmlfile.unlink
   end
   # rubocop:enable Metrics/AbcSize
 end
