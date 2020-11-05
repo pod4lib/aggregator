42d8f3ffd74be809c08b0ab0f8973f6f03824c17
Jessie Keck <jessie.keck@gmail.com>
2020-11-05 11:21:50 -0800

    Compress binary full-dumps using ruby Zlib.

--- app/jobs/download_job.rb
+++ app/jobs/download_job.rb
@@ -7,23 +7,27 @@ class DownloadJob < ApplicationJob
   def perform(organization)
     xmlfile = Tempfile.new("#{organization.slug}-marcxml")
     xmlwriter = MARC::XMLWriter.new(xmlfile)
-    file = Tempfile.new("#{organization.slug}-marc21", binmode: true)
+    binary_file = Tempfile.new("#{organization.slug}-marc21", binmode: true)
+    xml_compressor = Zlib::GzipWriter.new(xmlwriter)
+    binary_compressor = Zlib::GzipWriter.new(binary_file)
+
     organization.default_stream.uploads.each do |upload|
       upload.each_marc_record_metadata.each do |record|
-        file.write(record.marc.to_marc)
-        xmlwriter.write(record.marc)
+        xml_compressor.write(record.marc)
+        binary_compressor.write(record.marc.to_marc)
       end
     end
-    file.rewind
-    organization.full_dump_binary.attach(io: file, filename: "#{organization.slug}_#{Time.zone.today}")
+    binary_compressor.close
+    organization.full_dump_binary.attach(io: File.open(binary_file), filename: "#{organization.slug}_#{Time.zone.today}")
 
-    # we would use xmlwriter#close, but it annoyingly also closes the file handle on us..
-    xmlfile.write('</collection>')
-    xmlfile.rewind
-    organization.full_dump_xml.attach(io: xmlfile, filename: "#{organization.slug}_#{Time.zone.today}")
+    xml_compressor.write('</collection>')
+    xml_compressor.close
+    organization.full_dump_xml.attach(io: File.open(xmlfile), filename: "#{organization.slug}_#{Time.zone.today}")
   ensure
-    file.close
-    file.unlink
+    binary_compressor.close
+    xml_compressor.close
+    binary_file.close
+    binary_file.unlink
     xmlfile.close
     xmlfile.unlink
   end
