0ab796da993fda5be161cbe05ce773210eacc4a3
Chris Beer <cabeer@stanford.edu>
2020-11-09 11:35:26 -0800

    Open the mailer for configuration

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,3 +1,7 @@
 if Settings&.action_mailer&.default_url_options
   Aggregator::Application.config.action_mailer.default_url_options = Settings.action_mailer.default_url_options.try(:to_h) || {}
 end
+
+if Settings&.action_mailer&.url_options
+  Aggregator::Application.config.action_mailer.url_options = Settings.action_mailer.url_options.try(:to_h) || {}
+end
