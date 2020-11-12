fbaef7825ae9e059b8ac84c57da9b3889d102711
Jessie Keck <jessie.keck@gmail.com>
2020-11-12 15:02:30 -0800

    Try to get the filename from the ContentDisposition header from URL based uploads.

--- spec/jobs/attach_remote_file_to_upload_job_spec.rb
+++ spec/jobs/attach_remote_file_to_upload_job_spec.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'delegate'
+
+class RemoteUploadFixture < SimpleDelegator
+  attr_writer :meta
+
+  def meta
+    @meta ||= {}
+  end
+end
 
 RSpec.describe AttachRemoteFileToUploadJob, type: :job do
   before do
-    allow(URI).to receive(:open).and_return(
-      fixture_file_upload(Rails.root.join('spec/fixtures/1297245.marc'), 'application/octet-stream')
-    )
+    allow(URI).to receive(:open).and_return(fixture)
+  end
+
+  let(:fixture) do
+    RemoteUploadFixture.new(fixture_file_upload(Rails.root.join('spec/fixtures/1297245.marc'), 'application/octet-stream'))
   end
 
   let(:upload) { FactoryBot.create(:upload, files: [], url: 'http://example.com/1297245.marc') }
@@ -17,5 +28,17 @@ RSpec.describe AttachRemoteFileToUploadJob, type: :job do
     end.to change(upload.files, :attached?).to(true)
 
     expect(upload.files.first.blob.checksum).to eq 'iXNNqQC8bbzJqws9rdm09Q=='
+  end
+
+  context 'when the file has a ContentDisposition' do
+    before do
+      fixture.meta = { 'content-disposition' => 'attachement;filename="CD-filename.xml"' }
+    end
+
+    it 'gets the filename from the content disposition when present' do
+      expect(upload.files).to be_blank
+      described_class.perform_now(upload)
+      expect(upload.files.first.filename).to eq 'CD-filename.xml'
+    end
   end
 end
