49b7860bf8076a7d08230eb7b37b9ca3fc75562c
Jessie Keck <jessie.keck@gmail.com>
2020-11-20 08:28:04 -0800

    Add some helpful logging to db:seed (and inform that it may take some time).

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,24 @@ require_relative '../spec/support/marc_fixture_seed_fetcher'
 
 Upload.skip_callback(:commit, :after, :perform_extract_marc_record_metadata_job)
 
+puts "Seeding data from POD @ #{Settings.marc_fixture_seeds.host} (this may take a several minutes)"
 Settings.marc_fixture_seeds.organizations.each do |org|
+  puts "Fetching data from #{org}"
   organization = Organization.create(name: org, slug: org.downcase)
 
+  file_count = 0
   MarcFixtureSeedFetcher.fetch_uploads(org.downcase) do |upload, files|
     upload = organization.default_stream.uploads.build(name: upload['name'])
     files.map do |file|
+      file_count += 1
+      puts "Uploading file #{file['filename']} (#{file['download_url']})"
       io = URI.open(file['download_url'], 'Authorization' => "Bearer #{Settings.marc_fixture_seeds.token}")
       upload.files.attach(io: io, filename: file['filename'])
     end
     upload.save
   end
 
+  puts "Extracting metadata from #{file_count} files seeded from #{org}"
   # Run metadata extraction job serially
   organization.uploads.each do |upload|
     ExtractMarcRecordMetadataJob.perform_now(upload)
