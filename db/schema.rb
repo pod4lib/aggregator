2b454ffa736a6858896a5426ef0d56ec5fdebca0
Jack Reed <phillipjreed@gmail.com>
2020-12-02 09:37:58 -0700

    Support deletes to create MarcRecords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_154520) do
+ActiveRecord::Schema.define(version: 2020_12_02_005439) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -150,6 +153,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_154520) do
     t.bigint "length"
     t.bigint "index"
     t.string "checksum"
+    t.string "status"
     t.index ["file_id", "marc001"], name: "index_marc_records_on_file_id_and_marc001"
     t.index ["file_id"], name: "index_marc_records_on_file_id"
     t.index ["upload_id", "marc001"], name: "index_marc_records_on_upload_id_and_marc001"
@@ -264,12 +268,12 @@ ActiveRecord::Schema.define(version: 2020_11_20_154520) do
 
   create_table "versions", force: :cascade do |t|
     t.string "item_type", null: false
-    t.integer "item_id", limit: 8, null: false
+    t.bigint "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
-    t.text "object", limit: 1073741823
+    t.text "object"
     t.datetime "created_at"
-    t.text "object_changes", limit: 1073741823
+    t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
