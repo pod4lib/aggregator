649bf5a8e0937cd0834d3de104c98b271b5e75ed
Chris Beer <chris@cbeer.info>
2022-03-07 12:06:49 -0800

    Update to Rails 7

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -4,6 +4,6 @@ require 'rails_helper'
 
 RSpec.describe Role, type: :model do
   pending "add some examples to (or delete) #{__FILE__}" do
-    expect(role).to eq true
+    expect(role).to be true
   end
 end
--- spec/views/shared/flash_msg.html.erb_spec.rb
+++ spec/views/shared/flash_msg.html.erb_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'shared/_flash_msg.html.erb', type: :view do
+RSpec.describe 'shared/_flash_msg', type: :view do
   context 'with an alert flash' do
     before do
       flash[:alert] = 'This is an alert!'
