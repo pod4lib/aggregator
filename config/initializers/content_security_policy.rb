e2b11d7dc620efea1825aad86608902157514717
Marlo Longley <mlongley@stanford.edu>
2022-03-22 12:15:31 -0700

    Remove references to captcha

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,9 +9,9 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, :data
   policy.object_src  :none
-  policy.script_src  :self, 'https://www.google.com/recaptcha/', 'https://www.gstatic.com/recaptcha/', 'https://www.recaptcha.net'
+  policy.script_src  :self
   policy.style_src   :self, :https
-  policy.frame_src   :self, 'https://www.google.com/recaptcha/'
+  policy.frame_src   :self
   # If you are using webpack-dev-server then specify webpack-dev-server host
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
