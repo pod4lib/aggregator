1cdc62136f0b7188d8adf324fad7b3c656e0355c
Marlo Longley <mlongley@stanford.edu>
2022-04-01 17:17:50 -0400

    Remove charts from dashboard page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,10 +4,6 @@
 class DashboardController < ApplicationController
   skip_authorization_check only: [:uploads]
 
-  content_security_policy only: :uploads do |policy|
-    policy.style_src :self, :unsafe_inline
-  end
-
   def uploads
     @uploads = Upload.accessible_by(current_ability).order(updated_at: :desc).page(params[:page])
   end
