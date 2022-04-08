961d0b93b1d69a0130ad815c945500e20bbb4bab
Hurlyburly2 <douglas_simon@harvard.edu>
2022-04-08 12:12:31 -0400

    reverted cable and environments.rb. created docker versions of the files

--- config/environments/development-docker.rb
+++ config/environments/development-docker.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.active_job.queue_adapter = :sidekiq
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
