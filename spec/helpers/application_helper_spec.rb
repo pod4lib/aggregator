ab1ef875bd9267957aeb8f38837ec627613043ed
Katie Amaral <kathryn_amaral@harvard.edu>
2022-04-12 15:43:11 -0400

    Add spec test for helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -25,5 +25,20 @@ RSpec.describe ApplicationHelper do
       set_tab_class = helper.job_status_tabs_collapse(false, 3)
       expect(set_tab_class).to be('show')
     end
+
+    it 'applies class to collapse tab when tab expanded is true and job count is positive' do
+      set_tab_class = helper.job_status_tabs_collapse(true, 3)
+      expect(set_tab_class).to be('collapsed')
+    end
+
+    it 'applies class to collapse tab when tab expanded is true and job count is zero' do
+      set_tab_class = helper.job_status_tabs_collapse(true, 0)
+      expect(set_tab_class).to be('collapsed')
+    end
+
+    it 'applies class to collapse tab when tab expanded is false and job count is zero' do
+      set_tab_class = helper.job_status_tabs_collapse(false, 0)
+      expect(set_tab_class).to be('collapsed')
+    end
   end
 end
