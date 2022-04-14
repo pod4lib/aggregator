c34adb626e3bac8b9af63fa34476d01878993868
Katie Amaral <kathryn_amaral@harvard.edu>
2022-04-14 10:40:03 -0400

    Change to new hash syntax to fix tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@
 
 # Some site-wide dashboards
 class DashboardController < ApplicationController
-  authorize_resource :class => :controller
+  authorize_resource class: :controller
 
   def uploads
     @uploads = Upload.accessible_by(current_ability).order(updated_at: :desc).page(params[:page])
