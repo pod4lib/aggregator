812cf59ca5b5eb37a1eb47e525855fbe2db2cb78
Cory Lown <corylown@stanford.edu>
2022-04-20 12:09:44 -0400

    Remove pod-worker-stage-b.stanford.edu from deploy config

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,6 +1,5 @@
 server 'pod-stage.stanford.edu', user: 'pod', roles: %w(web db app background)
 server 'pod-worker-stage-a.stanford.edu', user: 'pod', roles: %w(background)
-server 'pod-worker-stage-b.stanford.edu', user: 'pod', roles: %w(background)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
