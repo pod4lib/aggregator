a1835ffaa20bd2017ec4685f85e474e76c8e5413
Hurlyburly2 <douglas_simon@harvard.edu>
2022-04-28 15:17:49 -0400

    rubocop6

--- spec/jobs/generate_interstream_delta_job_spec.rb
+++ spec/jobs/generate_interstream_delta_job_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe GenerateInterstreamDeltaJob, type: :job do
   let(:organization) { create(:organization) }
 
   before do
-      organization.default_stream.uploads << build(:upload, :binary_marc)
+    organization.default_stream.uploads << build(:upload, :binary_marc)
     organization.default_stream.uploads << build(:upload, :long_file)
     organization.default_stream.uploads << build(:upload, :binary_marc)
-    end
+  end
 
   it 'Does not create an interstream delta if there is no previous default stream' do
     GenerateFullDumpJob.perform_now(organization)
     described_class.perform_now(organization.default_stream)
-    expect(organization.default_stream.current_full_dump.interstream_delta).to be(nil)
+    expect(organization.default_stream.current_full_dump.interstream_delta).to be_nil
   end
 
   it 'Does not create an interstream delta if the current stream is missing a full dump' do
@@ -24,9 +24,10 @@ RSpec.describe GenerateInterstreamDeltaJob, type: :job do
     stream.make_default
 
     described_class.perform_now(organization.default_stream)
-    expect(organization.default_stream.current_full_dump.interstream_delta).to be(nil)
+    expect(organization.default_stream.current_full_dump.interstream_delta).to be_nil
   end
 
+  # rubocop:disable RSpec/ExampleLength
   it 'Creates an interstream delta with proper additions and deletes' do
     GenerateFullDumpJob.perform_now(organization)
 
@@ -35,7 +36,7 @@ RSpec.describe GenerateInterstreamDeltaJob, type: :job do
     organization.default_stream.save
 
     stream.make_default
-    
+
     stream.uploads << build(:upload, :marc_xml2)
     # Organization.find is used because it is loading an older version of the organization with the wrong
     # default stream. Unsure of the best way to 'refresh' so it's pulling the correct default
@@ -43,14 +44,13 @@ RSpec.describe GenerateInterstreamDeltaJob, type: :job do
 
     described_class.perform_now(stream)
 
-    expect(stream.current_full_dump.interstream_delta).not_to be(nil)
-
     # Deltas should contain one addition (we find two instances of 'record' in the xml for <record> and </record>)
     xml = stream.current_full_dump.interstream_delta.marcxml.open { |file| File.readlines(file) }.to_s
     expect(xml.scan(/record/).count).to be(2)
 
     # Deltas should contain two deletions
-    deletes = stream.current_full_dump.interstream_delta.deletes.open { |file | File.readlines(file) }
+    deletes = stream.current_full_dump.interstream_delta.deletes.open { |file| File.readlines(file) }
     expect(deletes.count).to be(2)
   end
+  # rubocop:enable RSpec/ExampleLength
 end
