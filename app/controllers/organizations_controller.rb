cf697bffa998b5af938304a2301c31a4b09470e5
Cory Lown <corylown@stanford.edu>
2022-04-29 10:20:11 -0400

    After updating org or provider details keep user on the same page. Fixes #726

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -64,10 +64,10 @@ class OrganizationsController < ApplicationController
   def update
     respond_to do |format|
       if @organization.update(organization_params)
-        format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
+        format.html { redirect_back_or_to @organization, notice: 'Organization was successfully updated.' }
         format.json { render :show, status: :ok, location: @organization }
       else
-        format.html { redirect_to @organization, alert: 'Organization could not be updated.' }
+        format.html { redirect_back_or_to @organization, alert: 'Organization could not be updated.' }
         format.json { render json: @organization.errors, status: :unprocessable_entity }
       end
     end
