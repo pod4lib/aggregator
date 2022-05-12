5ccdf2bef4a6f80be309ba15cb161b7190bb1ade
Marlo Longley <mlongley@stanford.edu>
2022-05-12 14:10:17 -0700

    Remove provider checkbox on form for existing organizations

--- spec/views/organizations/organization_details.html.erb_spec.rb
+++ spec/views/organizations/organization_details.html.erb_spec.rb
@@ -20,11 +20,18 @@ RSpec.describe 'organizations/organization_details', type: :view do
       assert_select 'input[name=?]', 'organization[name]'
       assert_select 'input[name=?]', 'organization[slug]'
       assert_select 'input[name=?]', 'organization[code]'
-      assert_select 'input[name=?]', 'organization[provider]'
       assert_select 'input[name=?]', 'organization[contact_email_attributes][email]'
     end
   end
   # rubocop:enable RSpec/ExampleLength
+
+  it 'renders provider checkbox on form if creating a new organization' do
+    allow(view).to receive(:can?).and_return(true)
+    allow(organization).to receive(:new_record?).and_return(true)
+    render
+
+    assert_select 'input[name=?]', 'organization[provider]'
+  end
 
   it 'renders a <dl> description list with 4 <dt> description terms' do
     # this part of the view is seen by org memebers
