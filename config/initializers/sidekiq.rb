392eade25e08cb3147b93682ffd6bdf4c3e2165b
Chris Beer <chris@cbeer.info>
2022-05-12 07:03:57 -0700

    Use environment variables for configuring sidekiq in deployed environments

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
 Sidekiq.configure_server do |config|
   config.logger.level = Object.const_get(Settings.sidekiq.logger_level)
+  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] } if ENV['SIDEKIQ_REDIS_URL'].present?
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] } if ENV['SIDEKIQ_REDIS_URL'].present?
 end
