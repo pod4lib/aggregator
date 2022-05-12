27fc317a579bd1f17357cf9bec2a919803ebbb2f
Chris Beer <cabeer@stanford.edu>
2022-05-12 10:12:22 -0700

    Fix rack-attack throttle logging

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,6 +6,6 @@ Rack::Attack.throttle("logins/ip", limit: 10, period: 12.hours) do |req|
   req.ip if req.post? && req.path.start_with?("/users/sign_in")
 end
 
-ActiveSupport::Notifications.subscribe("rack.attack") do |name, start, finish, request_id, req|
-  puts "Throttled #{req.env["rack.attack.match_discriminator"]}"
+ActiveSupport::Notifications.subscribe("rack.attack") do |name, start, finish, request_id, payload|
+  puts "Throttled #{payload[:request].env["rack.attack.match_discriminator"]}"
 end
