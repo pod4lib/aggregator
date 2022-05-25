0727a53864ea5fddf2f0b232e273e31ee43f4669
Cory Lown <corylown@stanford.edu>
2022-05-25 10:40:46 -0400

    Empty ActiveRecord::AssociationRelation returns true; must check for any?

--- spec/jobs/update_organization_statistics_job_spec.rb
+++ spec/jobs/update_organization_statistics_job_spec.rb
@@ -4,11 +4,14 @@ require 'rails_helper'
 
 RSpec.describe UpdateOrganizationStatisticsJob, type: :job do
   let(:organization) { create(:organization) }
+  let(:first_upload) { build(:upload, :binary_marc) }
+  let(:second_upload) { build(:upload, :binary_marc) }
+  let(:third_upload) { build(:upload, :binary_marc) }
 
   before do
-    organization.default_stream.uploads << build(:upload, :binary_marc)
-    organization.default_stream.uploads << build(:upload, :binary_marc)
-    organization.default_stream.uploads << build(:upload, :binary_marc)
+    organization.default_stream.uploads << first_upload
+    organization.default_stream.uploads << second_upload
+    organization.default_stream.uploads << third_upload
 
     # populate the MarcRecords index
     organization.default_stream.uploads.each { |u| ExtractMarcRecordMetadataJob.perform_now(u) }
@@ -30,5 +33,19 @@ RSpec.describe UpdateOrganizationStatisticsJob, type: :job do
       record_count: 3, unique_record_count: 1,
       file_count: 3, file_size: 4221
     )
+  end
+
+  it 'calculates statistics when there are no newer uploads' do
+    described_class.perform_now(organization, organization.default_stream, third_upload)
+
+    expect(organization.latest_statistics).to have_attributes(
+      record_count: 3, unique_record_count: 1,
+      file_count: 3, file_size: 4221
+    )
+  end
+
+  it 'does not calculate statistics if there are newer uploads' do
+    described_class.perform_now(organization, organization.default_stream, first_upload)
+    expect(organization.statistics).to be_empty
   end
 end
