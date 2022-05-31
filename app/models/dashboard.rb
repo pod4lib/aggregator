c59cfda2247eb182b08c47e9cb54840b584cba2c
Marlo Longley <mlongley@stanford.edu>
2022-05-31 11:33:31 -0700

    Only load Activity summary tabs on page 1 of uploads

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -3,26 +3,18 @@
 # Summary of recent activity across the whole app
 class Dashboard
   def recent_uploads_by_provider
-    Rails.cache.fetch(:recent_uploads_by_provider, expires_in: 1.hour) do
-      Upload.recent.where('created_at > ?', 30.days.ago).group_by(&:organization)
-    end
+    Upload.recent.where('created_at > ?', 30.days.ago).group_by(&:organization)
   end
 
   def job_status_groups_by_provider
-    Rails.cache.fetch(:job_status_groups_by_provider, expires_in: 1.hour) do
-      Organization.providers.index_with { |org| org.default_stream.job_tracker_status_groups }
-    end
+    Organization.providers.index_with { |org| org.default_stream.job_tracker_status_groups }
   end
 
   def normalized_data_by_provider
-    Rails.cache.fetch(:normalized_data_by_provider, expires_in: 1.hour) do
-      Organization.providers.index_with { |org| org.default_stream.normalized_dumps.full_dumps.last }
-    end
+    Organization.providers.index_with { |org| org.default_stream.normalized_dumps.full_dumps.last }
   end
 
   def users_by_organization
-    Rails.cache.fetch(:users_by_provider, expires_in: 1.hour) do
-      Organization.all.index_with(&:users)
-    end
+    Organization.all.index_with(&:users)
   end
 end
