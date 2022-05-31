4f7733983116ddb39b772a55033438aa24ce3eea
Marlo Longley <mlongley@stanford.edu>
2022-05-31 11:49:25 -0700

    Add tests for new pagination based layout

--- spec/views/dashboard/summary.html.erb_spec.rb
+++ spec/views/dashboard/summary.html.erb_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe 'dashboard/summary', type: :view do
   it 'renders the tab layout' do
     render
 
-    expect(rendered).to have_css('ul#summary-tabs > li', count: 5)
+    expect(rendered).to have_css('ul#summary-tabs > li', count: 4)
     expect(rendered).to have_css('div#summary-tabs-content > div#uploads-pane', count: 1)
     expect(rendered).to have_css('div#summary-tabs-content > div#job-status-pane', count: 1)
     expect(rendered).to have_css('div#summary-tabs-content > div#normalized-data-pane', count: 1)
@@ -55,5 +55,10 @@ RSpec.describe 'dashboard/summary', type: :view do
 
     expect(rendered).to have_css('table#jobs-table > tbody > tr > td > a', text: job_tracker_active.created_at)
     expect(rendered).to have_css('table#jobs-table > tbody > tr > td > a', text: job_tracker_attention.created_at)
+  end
+
+  it 'does not render the activity summary tabs on page 2 of uploads' do
+    assign(:uploads, Kaminari.paginate_array(uploads1).page(2))
+    expect(rendered).not_to have_css('ul#summary-tabs')
   end
 end
