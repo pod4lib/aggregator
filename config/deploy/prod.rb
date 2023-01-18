4ccc61cccc5fa11ac9cfb78a166f6a670938a711
Justin Coyne <jcoyne@justincoyne.com>
2023-01-18 12:43:55 -0600

    Configure prod deploy to have worker machines

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,6 @@
-server 'pod.stanford.edu', user: 'pod', roles: %w(web db app background)
+server 'pod.stanford.edu', user: 'pod', roles: %w(web db app background) # remove background here when we switch to the pod-redis-prod-a
+server 'pod-worker-prod-a.stanford.edu', user: 'pod', roles: %w(background)
+server 'pod-worker-prod-b.stanford.edu', user: 'pod', roles: %w(background)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
