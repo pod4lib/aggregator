774f0cf07fd97115b5dc4d4de1c60dcab043520d
Cory Lown <corylown@stanford.edu>
2023-01-24 20:31:04 -0500

    Change default redis db numbers to start at 0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, { url: ENV.fetch("CACHE_REDIS_URL") { "redis://localhost:6379/2" } }
+  config.cache_store = :redis_cache_store, { url: ENV.fetch("CACHE_REDIS_URL") { "redis://localhost:6379/1" } }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter     = :sidekiq
