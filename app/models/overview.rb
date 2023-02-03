71ec2c1e835f47a9e891c2e9cff96cd47eb70a46
Cory Lown <corylown@stanford.edu>
2023-02-03 16:58:37 -0500

    Calculate pod total record stats from organization default streams

--- app/models/overview.rb
+++ app/models/overview.rb
@@ -25,12 +25,12 @@ class Overview
 
   # total number of MARC records in provider default streams
   def total_records
-    providers.map(&:latest_statistics).sum(&:record_count)
+    providers.sum { |p| p.default_stream.statistic&.record_count || 0 }
   end
 
   # number of unique MARC records (by 001 field) in provider default streams
   def unique_records
-    providers.map(&:latest_statistics).sum(&:unique_record_count)
+    providers.sum { |p| p.default_stream.statistic&.unique_record_count || 0 }
   end
 
   # most recent 3 files uploaded to the user's organization's default stream
