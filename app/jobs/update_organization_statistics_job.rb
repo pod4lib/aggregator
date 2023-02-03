108e21151aa4a43d9dab5667765067dbc52a09fc
Cory Lown <corylown@stanford.edu>
2023-02-03 16:57:11 -0500

    Stop calculating organization statistics. We'll use the default stream stats instead.

--- app/jobs/update_organization_statistics_job.rb
+++ app/jobs/update_organization_statistics_job.rb
@@ -18,33 +18,13 @@ class UpdateOrganizationStatisticsJob < ApplicationJob
     # short-circuit if the stream's statistics have already been updated today
     return if stream&.statistic&.updated_at&.today?
 
-    generate_statistics!(organization, stream)
+    generate_statistics!(stream)
   end
   # rubocop:enable Metrics/CyclomaticComplexity
 
-  def generate_statistics!(organization, stream)
+  def generate_statistics!(stream)
     stream_statistics = stream.statistic || stream.create_statistic
     stream_statistics.update(date: Time.zone.today, **calculate_stream_statistics(stream))
-
-    # We need to reload the resource so that the organization stats
-    # include the latest calculated default_stream.statistics.
-    organization.reload
-
-    org_statistics = organization.statistics.find_or_create_by(date: Time.zone.today)
-
-    org_statistics.update(
-      date: Time.zone.today,
-      **calculate_organization_statistics(organization)
-    )
-  end
-
-  def calculate_organization_statistics(context)
-    {
-      unique_record_count: context.marc_records.distinct.count(:marc001),
-      record_count: context.marc_records.size,
-      file_count: context.default_stream&.statistic&.file_count,
-      file_size: context.default_stream&.statistic&.file_size
-    }
   end
 
   def calculate_stream_statistics(context)
--- spec/jobs/update_organization_statistics_job_spec.rb
+++ spec/jobs/update_organization_statistics_job_spec.rb
@@ -17,15 +17,6 @@ RSpec.describe UpdateOrganizationStatisticsJob do
     organization.default_stream.uploads.each { |u| ExtractMarcRecordMetadataJob.perform_now(u) }
   end
 
-  it 'calculates organization-level statistics' do
-    described_class.perform_now(organization)
-
-    expect(organization.latest_statistics).to have_attributes(
-      record_count: 3, unique_record_count: 1,
-      file_count: 3, file_size: 4221
-    )
-  end
-
   it 'calculates stream-level statistics' do
     described_class.perform_now(organization)
 
@@ -38,7 +29,7 @@ RSpec.describe UpdateOrganizationStatisticsJob do
   it 'calculates statistics when there are no newer uploads' do
     described_class.perform_now(organization, organization.default_stream, third_upload)
 
-    expect(organization.latest_statistics).to have_attributes(
+    expect(organization.default_stream.statistic).to have_attributes(
       record_count: 3, unique_record_count: 1,
       file_count: 3, file_size: 4221
     )
@@ -46,6 +37,6 @@ RSpec.describe UpdateOrganizationStatisticsJob do
 
   it 'does not calculate statistics if there are newer uploads' do
     described_class.perform_now(organization, organization.default_stream, first_upload)
-    expect(organization.statistics).to be_empty
+    expect(organization.default_stream.statistic).to be_nil
   end
 end
