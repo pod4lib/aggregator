05d31091547973fb3e36b123c8c500c072c4bcf9
Chris Beer <cabeer@stanford.edu>
2025-10-13 11:11:17 -0700

    Update schema version.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_09_132420) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_10_101010) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
