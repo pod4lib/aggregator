aca9742e41b06c2e0cec1b0084a8bee7e3f9aea3
Cory Lown <corylown@stanford.edu>
2025-10-13 10:01:54 -0400

    Do not save status check visits

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -4,6 +4,11 @@ OkComputer.mount_at = 'status'
 
 OkComputer::Registry.register 'redis', OkComputer::RedisCheck.new(url: ENV.fetch('SIDEKIQ_REDIS_URL') { 'redis://localhost:6379/0' })
 
+# Do not track ahoy visits to status check page
+Rails.application.config.to_prepare do
+  OkComputer::OkComputerController.skip_before_action :track_ahoy_visit
+end
+
 # check activestorage by uploading + downloading a file
 class ActiveStorageCheck < OkComputer::Check
   def check
