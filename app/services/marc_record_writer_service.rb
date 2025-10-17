4d7df9d33afacc64ca2c42f8d17503c83fc93c10
Chris Beer <cabeer@stanford.edu>
2025-10-17 08:32:57 -0700

    Pull the OAI writer back into MarcRecordWriterService.

--- app/services/marc_record_writer_service.rb
+++ app/services/marc_record_writer_service.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 # Utility class for serializing MARC records to files
-class MarcRecordWriterService
-  attr_reader :base_name, :files
+class MarcRecordWriterService # rubocop:disable Metrics/ClassLength
+  attr_reader :base_name, :files, :dump, :now
 
-  def initialize(base_name = nil, files: { errata: nil, marcxml: nil, marc21: nil, deletes: nil })
+  def initialize(base_name = nil, dump:, now: Time.zone.now, files: { errata: nil, marcxml: nil, marc21: nil, deletes: nil })
     @base_name = base_name
+    @dump = dump
+    @now = now
     @files = files.compact
     @opened_files = []
     @writers = {}
@@ -16,10 +18,12 @@ class MarcRecordWriterService
 
     write_marc21_record(record)
     write_marcxml_record(record)
+    write_oai_record(record)
   end
 
   def write_delete(record)
     deletes_writer.puts(record.marc001)
+    oai_writer.write_delete(record)
   end
 
   def write_errata(message)
@@ -27,20 +31,35 @@ class MarcRecordWriterService
   end
 
   def finalize
+    oai_writer.finalize
+
     @writers.each_value(&:close)
+
+    files.each do |as, file|
+      dump.public_send(as).attach(io: File.open(file), filename: human_readable_filename(base_name, as))
+    end
   end
 
   def close
     @opened_files.each(&:close)
+
+    oai_writer.close
   end
 
   def unlink
     @opened_files.each(&:unlink)
+    oai_writer.unlink
 
     FileUtils.rm_rf tempdir
   end
 
   private
+
+  def write_oai_record(record)
+    oai_writer.write_marc_record(record)
+  rescue StandardError => e
+    write_errata("#{record.marc001}: #{e}")
+  end
 
   def write_marc21_record(record)
     writer(:marc21).write(split_marc(record.augmented_marc))
@@ -68,6 +87,10 @@ class MarcRecordWriterService
 
   def deletes_writer
     @writers[:deletes] ||= file(:deletes)
+  end
+
+  def oai_writer
+    @writers[:oai] ||= ChunkedOaiMarcRecordWriterService.new(base_name, dump: dump, now: now)
   end
 
   def gzipped_temp_file(name)
@@ -101,5 +124,20 @@ class MarcRecordWriterService
 
   def tempdir
     @tempdir ||= Dir.mktmpdir(base_name || 'marc_record_writer', Settings.marc_record_writer_tmpdir || Dir.tmpdir)
+  end
+
+  def human_readable_filename(base_name, file_type)
+    as = case file_type
+         when :deletes
+           'deletes.del.txt'
+         when :marc21
+           'marc21.mrc.gz'
+         when :marcxml
+           'marcxml.xml.gz'
+         else
+           "#{file_type}.gz"
+         end
+
+    "#{base_name}-#{as}"
   end
 end
