6772522b8ca9724849c5ed55b9271e48f8c20428
Chris Beer <cabeer@stanford.edu>
2025-10-17 08:32:57 -0700

    Pull the OAI writer back into MarcRecordWriterService.

--- app/jobs/generate_full_dump_job.rb
+++ app/jobs/generate_full_dump_job.rb
@@ -28,8 +28,7 @@ class GenerateFullDumpJob < ApplicationJob
     full_dump = organization.default_stream.normalized_dumps.build(last_full_dump_at: now, last_delta_dump_at: now)
 
     base_name = "#{organization.slug}-#{Time.zone.today}-full"
-    writer = MarcRecordWriterService.new(base_name)
-    oai_writer = ChunkedOaiMarcRecordWriterService.new(base_name, dump: full_dump, now: now)
+    writer = MarcRecordWriterService.new(base_name, dump: full_dump, now: now)
 
     begin
       NormalizedMarcRecordReader.new(uploads).each_slice(1000) do |records|
@@ -42,18 +41,12 @@ class GenerateFullDumpJob < ApplicationJob
           next if record.status == 'delete'
 
           writer.write_marc_record(record)
-          oai_writer.write_marc_record(record)
         end
 
         progress.increment(records.length)
       end
 
-      oai_writer.finalize
       writer.finalize
-
-      writer.files.each do |as, file|
-        full_dump.public_send(as).attach(io: File.open(file), filename: human_readable_filename(base_name, as))
-      end
 
       # Add a timestamp when the dump is saved at the end of the job to indicate
       # it is complete, should supercede the previous full dump, and is ready for harvesting.
@@ -64,25 +57,7 @@ class GenerateFullDumpJob < ApplicationJob
     ensure
       writer.close
       writer.unlink
-
-      oai_writer.close
-      oai_writer.unlink
     end
   end
   # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
-
-  def human_readable_filename(base_name, file_type)
-    as = case file_type
-         when :deletes
-           'deletes.del.txt'
-         when :marc21
-           'marc21.mrc.gz'
-         when :marcxml
-           'marcxml.xml.gz'
-         else
-           "#{file_type}.gz"
-         end
-
-    "#{base_name}-#{as}"
-  end
 end
