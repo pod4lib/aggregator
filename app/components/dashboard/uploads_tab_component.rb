c91b0ee9fdeefbd4c36ac741f4a8ee3933aea117
Chris Beer <cabeer@stanford.edu>
2025-10-24 11:39:20 -0700

    Calculate statuses per organization on the activities page

--- app/components/dashboard/uploads_tab_component.rb
+++ app/components/dashboard/uploads_tab_component.rb
@@ -7,37 +7,23 @@ module Dashboard
 
     delegate :local_time, :datetime_display_format, to: :helpers
 
-    def initialize(uploads:)
-      @uploads = uploads
-      super()
-    end
-
     def recent_uploads_by_provider
       @recent_uploads_by_provider ||= Organization.providers.index_with do |org|
         org.uploads.recent.where('uploads.created_at > ?', 30.days.ago)
       end
     end
 
-    def last_successful_upload_date
-      @last_successful_upload_date ||=
-        @uploads.find { |upload| files_status(upload) == :completed }&.created_at
-    end
-
-    def status
-      @status ||= best_status
-    end
-
-    private
-
     # Return the most successful status level given a set up uploads
-    def best_status
-      statuses = @uploads.map { |upload| files_status(upload) }.uniq
+    def best_status(uploads)
+      statuses = uploads.map { |upload| files_status(upload) }.uniq
 
       return :completed if statuses.include? :completed
       return :needs_attention if statuses.include? :needs_attention
 
       :failed if statuses.include? :failed
     end
+
+    private
 
     # Status criteria outlined in https://github.com/pod4lib/aggregator/issues/674
     # Completed - When all files in the upload are flagged as valid MARC or deletes
