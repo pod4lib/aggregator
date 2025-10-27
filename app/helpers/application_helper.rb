5d91cd11c0f6a8423071afd833f281cbf9ab1b05
Chris Beer <cabeer@stanford.edu>
2025-10-27 08:16:57 -0700

    Convert stream_status_badge helper to a component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,39 +13,4 @@ module ApplicationHelper
   def datetime_display_format
     '%B %e, %Y %l:%M%P %Z'
   end
-
-  def hidden_time(time, time_type)
-    local_time(time, format: datetime_display_format, class: "hidden-popover-time d-none #{time_type}")
-  end
-
-  def badge_popover_text(stream)
-    if stream.default?
-      concat(hidden_time(stream.created_at, 'default'))
-    else
-      concat(hidden_time(stream.created_at, 'start'))
-      concat(hidden_time(stream.updated_at, 'end'))
-    end
-  end
-
-  def stream_status_badge(stream)
-    badge_label = case stream.status
-                  when 'default' then 'Default'
-                  when 'previous-default' then 'Previous default'
-                  end
-
-    return if badge_label.blank?
-
-    # popovers are added to <button> elements for accessibility.
-    # See https://getbootstrap.com/docs/5.0/components/popovers
-    content_tag(:button,
-                href: '#',
-                class: "badge text-dark btn #{stream.default? ? 'btn-info' : 'btn-warning'}",
-                'data-bs-toggle': 'popover',
-                'data-bs-placement': 'top',
-                'data-bs-html': 'true') do
-      concat(badge_label)
-      concat(content_tag(:i, nil, class: 'bi bi-info-circle-fill text-dark'))
-      badge_popover_text(stream)
-    end
-  end
 end
