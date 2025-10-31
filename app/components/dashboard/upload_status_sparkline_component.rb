1b96c06098e6f36a1bbe1e08604e97ab2deed360
Chris Beer <cabeer@stanford.edu>
2025-10-31 11:13:33 -0700

    Add sparkline-style display of the 30-day history, fixes #679

---
+++ app/components/dashboard/upload_status_sparkline_component.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Dashboard
+  # Uploads activity table showing recent upload statuses
+  class UploadStatusSparklineComponent < ViewComponent::Base
+    def initialize(uploads:)
+      @uploads = uploads
+      super()
+    end
+
+    def sparklines # rubocop:disable Metrics/AbcSize
+      safe_join((30.days.ago.to_date..Time.zone.today).map do |date|
+        daily_uploads = uploads_by_date[date] || []
+        if daily_uploads.empty?
+          tag.span('', class: 'sparkline-bar', aria: { hidden: true })
+        else
+          # Show the worst status of the day
+          worst_upload = daily_uploads.min_by do |upload|
+            case upload.metadata_status
+            when 'success' then 0
+            when 'invalid' then 1
+            else 2
+            end
+          end
+
+          upload_sparkline_tag(worst_upload)
+        end
+      end)
+    end
+
+    def uploads_by_date
+      @uploads.group_by { |upload| upload.created_at.to_date }
+    end
+
+    def upload_sparkline_tag(upload)
+      case upload.metadata_status
+      when 'success'
+        tag.span('', class: 'border sparkline-bar bg-success', title: "Success on #{upload.created_at.strftime('%Y-%m-%d')}")
+      when 'invalid'
+        tag.span('', class: 'border sparkline-bar bg-danger', title: "Failed on #{upload.created_at.strftime('%Y-%m-%d')}")
+      end
+    end
+  end
+end
