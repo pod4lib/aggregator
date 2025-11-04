acc600bb23929480d9e479ef28c32cb7d3afa726
Chris Beer <cabeer@stanford.edu>
2025-11-04 13:13:04 -0800

    Remove upload summary.

--- spec/components/dashboard/summary_component_spec.rb
+++ spec/components/dashboard/summary_component_spec.rb
@@ -3,14 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Dashboard::SummaryComponent, type: :component do
-  subject(:rendered) { Capybara::Node::Simple.new(render_inline(described_class.new(uploads: uploads)).to_html) }
+  subject(:rendered) { Capybara::Node::Simple.new(render_inline(described_class.new).to_html) }
 
   let(:user) { create(:user) }
-  let(:provider) { create(:organization, name: 'provider1') }
-  let(:uploads_list) { create_list(:upload, 2, :multiple_files, stream: provider.default_stream) }
-
-  let(:first_page_uploads)  { Kaminari.paginate_array(uploads_list).page(1) }
-  let(:second_page_uploads) { Kaminari.paginate_array(uploads_list).page(2) }
 
   # rubocop:disable RSpec/AnyInstance
   before do
@@ -19,8 +14,6 @@ RSpec.describe Dashboard::SummaryComponent, type: :component do
   # rubocop:enable RSpec/AnyInstance
 
   context 'with page 1 of uploads' do
-    let(:uploads) { first_page_uploads }
-
     # rubocop:disable RSpec/MultipleExpectations
     it 'renders all summary tabs' do
       expect(rendered).to have_css('ul#summary-tabs > li', count: 3)
@@ -29,19 +22,5 @@ RSpec.describe Dashboard::SummaryComponent, type: :component do
       expect(rendered).to have_css('div#summary-tabs-content > div#users-pane')
     end
     # rubocop:enable RSpec/MultipleExpectations
-
-    it 'renders upload activity table with upload name and file name' do
-      expect(rendered).to have_css('table#upload-activity > tbody > tr > td', text: uploads_list.first.name)
-      expect(rendered).to have_css('table#upload-activity > tbody > tr > td',
-                                   text: uploads_list.first.files.first.filename.to_s)
-    end
-  end
-
-  context 'with page 2 of uploads' do
-    let(:uploads) { second_page_uploads }
-
-    it 'does not render the summary tabs' do
-      expect(rendered).to have_no_css('ul#summary-tabs')
-    end
   end
 end
