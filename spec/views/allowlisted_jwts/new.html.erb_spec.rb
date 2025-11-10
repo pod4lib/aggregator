8440b6d92b2f1009d17d61fca158644cf3760e0d
Cory Lown <corylown@stanford.edu>
2025-11-10 14:44:18 -0500

    Let consumer only orgs only create download tokens

--- spec/views/allowlisted_jwts/new.html.erb_spec.rb
+++ spec/views/allowlisted_jwts/new.html.erb_spec.rb
@@ -8,20 +8,32 @@ RSpec.describe 'allowlisted_jwts/new' do
   before do
     assign(:allowlisted_jwt, AllowlistedJwt.new)
     assign(:organization, organization)
+    render
   end
 
   it 'renders new upload form' do
-    render
     assert_select 'form[action=?][method=?]', organization_allowlisted_jwts_path(organization), 'post' do
       assert_select 'input[name=?]', 'allowlisted_jwt[label]'
     end
   end
 
   it 'has options for different token scopes' do
-    render
     assert_select 'form[action=?][method=?]', organization_allowlisted_jwts_path(organization), 'post' do
       assert_select 'input[name=?][value=?]', 'allowlisted_jwt[scope]', 'download'
       assert_select 'input[name=?][value=?]', 'allowlisted_jwt[scope]', 'upload'
+      assert_select 'input[name=?][value=?]', 'allowlisted_jwt[scope]', 'all'
+    end
+  end
+
+  context 'when organization is not a provider' do
+    let(:organization) { create(:organization, :consumer) }
+
+    it 'only has option for download-only token scope' do
+      assert_select 'form[action=?][method=?]', organization_allowlisted_jwts_path(organization), 'post' do
+        assert_select 'input[name=?][value=?]', 'allowlisted_jwt[scope]', 'download'
+        assert_select 'input[name=?][value=?]', 'allowlisted_jwt[scope]', 'upload', count: 0
+        assert_select 'input[name=?][value=?]', 'allowlisted_jwt[scope]', 'all', count: 0
+      end
     end
   end
 end
